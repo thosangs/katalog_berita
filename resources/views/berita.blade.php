f79bf301e7a3456e7ad9ad9e722d012225329ef9
blankon123 <thosan.girisona@gmail.com>
2020-06-27 19:41:08 +0700

    Debug Header

--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="icon" href="{{ asset('favicon.svg') }}" />
-  <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>Aktris - Aplikasi Katalog Berita Statistik</title>
 
   <link rel="stylesheet" type="text/css"
     href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
