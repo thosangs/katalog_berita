84a8267b3050efbd5f8cb08c255db2d43d3bda3f
Thosan Girisona Suganda <thosan.suganda@bps.go.id>
2020-06-27 19:39:43 +0700

    Add Open Graph on Header

--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -6,6 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="icon" href="{{ asset('favicon.svg') }}" />
+
+  <meta property=”og:title” content='Aktris - Aplikasi Katalog Berita Statistik' />
+  <meta property=”og:url” content=”{{ route('home') }}” />
+  <meta property=”og:type” content=”website” />
+  <meta property=”og:description” content='Aplikasi sederhana hasil iseng-iseng berhadiah Penampil Berita Kalteng yang berhasil si-crawl oleh Kofax Kapow yang update setiap hari 😁' />
+  <meta property=”og:image” content=”{{ asset('favicon.svg') }}” />
+
   <title>{{ config('app.name', 'Laravel') }}</title>
 
   <link rel="stylesheet" type="text/css"
